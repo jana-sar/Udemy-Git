bda142678cbf6c7ed991c5db9e9f2822df209406
jana-sar <122573645+jana-sar@users.noreply.github.com>
2023-02-04 01:06:16 +0300

    Add files via upload

---
+++ webDevelopment/try.js
@@ -0,0 +1,12 @@
+function change() {
+  var dd1=document.getElementById('d1');
+  var dd2=document.getElementById('d2');
+  var dd3=document.getElementById('d3');
+
+  dd1.className="blackcolor";
+  dd2.className="blackcolor";
+  dd3.className="blackcolor";
+
+}
+
+  
